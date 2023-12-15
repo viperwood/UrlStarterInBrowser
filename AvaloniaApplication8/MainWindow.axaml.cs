fc744b63d5a8180c5253ce747cd2658791cc8ff6
prdb <prdb@spb-rtk.ru>
2023-12-15 12:06:58 +0300

    UrlStarter2

--- AvaloniaApplication8/MainWindow.axaml.cs
+++ AvaloniaApplication8/MainWindow.axaml.cs
@@ -11,14 +11,14 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
     }
 
     private void Tab(object? sender, RoutedEventArgs e)
     {
-
-        string url = "http://www.microsoft.com";
-        string brayser = @"C:\Program Files (x86)\Internet Explorer\iexplore.exe";
-        Process.Start(brayser,url);
-
+        Process process = new Process();
+        process.StartInfo.UseShellExecute = true;
+        process.StartInfo.FileName = "https://www.youtube.com/watch?v=lHpYyYtkmrw";
+        process.Start();
     }
 }
